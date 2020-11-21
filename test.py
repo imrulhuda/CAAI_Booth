7d05a78b546c6cb830240c9fa1717c0296871c48
Imrul Huda <imrul66@gmail.com>
2020-11-21 00:50:20 -0500

    Add files via upload

--- test.py
+++ test.py
@@ -20,18 +20,13 @@ class ImageSequence(Sequence):
         imgs = []
         ages = []
         for _, row in self.df.iloc[sample_indices].iterrows():
-            # print(row)
-            # print(str(self.img_dir.joinpath(row["img_paths"])))
             img = cv2.imread(self.img_dir + row["img_paths"])
-            # try:
             img = cv2.resize(img, (self.img_size, self.img_size))
-            # except Exception as e:
-            #     print(str(e))
-            #     continue
             imgs.append(img)
             ages.append(row["ages"])
         imgs = np.asarray(imgs)
         ages = np.asarray(ages)
+        # Unlike the sequence of the training we only return the images
         return imgs
     def __len__(self):
         return len(self.df) // self.batch_size
@@ -40,15 +35,21 @@ class ImageSequence(Sequence):
 
 
 def get_mae(y_hat,y):
+    '''
+    returns the mean absolut error of 02 array
+    '''
     return np.sum(np.absolute(y_hat - y))/y.size
-
 
 model = tf.keras.models.load_model("age_vgg16_224.h5")
 wiki = pd.read_csv("meta/wiki.csv")
+# shuffle
 wiki = wiki.sample(frac = 1)
+# taking a very small sample
 test = wiki.sample(n = 500)
 test_gen = ImageSequence(test)
+# saving the probability outputs
 classes = model.predict(test_gen)
+# extracting the age with highest probability
 predictions = tf.argmax(input = classes, axis = 1)
 print(get_mae(predictions, test['ages']))
 
